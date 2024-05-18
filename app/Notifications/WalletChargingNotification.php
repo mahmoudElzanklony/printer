b8b024eb34c741161229d834c8c2af702a9029c4
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-05-19 01:34:34 +0300

    try to test wallet notificaiton

--- app/Notifications/WalletChargingNotification.php
+++ app/Notifications/WalletChargingNotification.php
@@ -59,6 +59,7 @@ class WalletChargingNotification extends Notification
 
         SendEmail::send('تم شحن رصيد المحفظه في '.env('APP_NAME'),'تم شحن رصيد المحفظه بقيمه '.$this->user->wallet - $this->old_wallet.' و اصبح الرصيد الحالي هو '.$this->user->wallet,'','',$this->user->email);
         SendEmail::send('The wallet balance has been charged at '.env('APP_NAME'),'The wallet balance has been charged '.$this->user->wallet - $this->old_wallet.' The current balance becomes '.$this->user->wallet,'','',$this->user->email);
+        return true;
     }
 
     /**
