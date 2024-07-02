70bb8ceb870da6e7ee51a180c2a0b574c5734212
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-07-02 19:00:31 +0300

    upload

--- app/Http/Controllers/ServicesControllerResource.php
+++ app/Http/Controllers/ServicesControllerResource.php
@@ -22,7 +22,7 @@ class ServicesControllerResource extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:sanctum')->except('index');
+        $this->middleware('auth:sanctum')->except('index','show');
     }
     public function index()
     {
