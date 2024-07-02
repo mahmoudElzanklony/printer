f83a6d9c737fd73fc5df4c5925cd949e9d771d12
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2024-07-02 18:59:46 +0300

    upload

--- app/Http/Controllers/PropertiesControllerResource.php
+++ app/Http/Controllers/PropertiesControllerResource.php
@@ -16,7 +16,7 @@ class PropertiesControllerResource extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:sanctum')->except('index');
+        $this->middleware('auth:sanctum')->except('index','show');
     }
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/PropertiesHeadingControllerResource.php
+++ app/Http/Controllers/PropertiesHeadingControllerResource.php
@@ -16,7 +16,7 @@ class PropertiesHeadingControllerResource extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:sanctum')->except('index');
+        $this->middleware('auth:sanctum')->except('index','show');
     }
     /**
      * Display a listing of the resource.
