c4223de4c59496931992715c81de821713cb2981
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-01-12 17:24:44 +0200

    edit

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -6,6 +6,7 @@ use App\Models\categories;
 use App\Models\saved_locations;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use PharIo\Version\Exception;
 
 class UserResource extends JsonResource
 {
@@ -16,29 +17,33 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $data = [
-            'id'=>$this->id,
-            'username'=>$this->username,
-            'email'=>$this->email,
-            'verified_at'=>$this->phone_verified_at,
-            'phone'=>$this->phone,
-            'otp'=>$this->otp_secret,
-            'wallet'=>$this->wallet,
-            'role'=>$this->roles->pluck('name')[0],
-            'created_at'=>$this->created_at->format('Y-m-d H:i:s')
-        ];
-        if(isset($this->default_location)){
-            $data['saved_location'] = SavedLocationResource::make($this->default_location);
+        try{
+            $data = [
+                'id'=>$this->id,
+                'username'=>$this->username,
+                'email'=>$this->email,
+                'verified_at'=>$this->phone_verified_at,
+                'phone'=>$this->phone,
+                'otp'=>$this->otp_secret,
+                'wallet'=>$this->wallet,
+                'role'=>$this->roles->pluck('name')[0],
+                'created_at'=>$this->created_at->format('Y-m-d H:i:s')
+            ];
+            if(isset($this->default_location)){
+                $data['saved_location'] = SavedLocationResource::make($this->default_location);
+            }
+            if(isset($this->default_category_id)){
+                $data['default_category_id'] = $this->default_category_id;
+            }
+            if(isset($this->default_service_id)){
+                $data['default_service_id'] = $this->default_service_id;
+            }
+            if(isset($this->token)){
+                $data['token'] = $this->token;
+            }
+            return $data;
+        }catch (Exception $exception){
+            dd($this->id);
         }
-        if(isset($this->default_category_id)){
-            $data['default_category_id'] = $this->default_category_id;
-        }
-        if(isset($this->default_service_id)){
-            $data['default_service_id'] = $this->default_service_id;
-        }
-        if(isset($this->token)){
-            $data['token'] = $this->token;
-        }
-        return $data;
     }
 }
