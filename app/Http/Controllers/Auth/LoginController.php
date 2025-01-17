391a3e1a0ba53cb90562e3c7a77d875a49ae670a
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-01-17 22:19:34 +0200

    add icon info to index , show at controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,7 +21,7 @@ class LoginController extends Controller
             $user = User::query()->where('email',$data['email'])->first();
         }else if(request()->filled('phone') && request()->filled('password')){
             $data = ['phone'=>request('phone') , 'password'=>request('password')];
-
+            dd(auth('web')->attempt($data));
             $check = auth('web')->attempt($data);
 
             if($check){
