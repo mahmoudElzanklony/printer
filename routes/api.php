2d684d71ae79bffa0e53a775a80168044657951c
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-01-21 23:04:58 +0200

    check env exists

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,8 @@ use App\Http\Controllers\ContactsControllerResource;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
 Route::group(['middleware'=>'changeLang'],function (){
     // auth module
     Route::group(['prefix'=>'/auth'],function (){
