c1f852f0f26c388e788b61a0eda51d6215bb2681
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-02-10 20:47:43 +0200

    edit check max before save properties

--- app/Services/CheckMaxBeforeSaveService.php
+++ app/Services/CheckMaxBeforeSaveService.php
@@ -7,23 +7,25 @@ use App\Models\saved_properties_settings;
 
 class CheckMaxBeforeSaveService
 {
-    public static function execute_saved_locations(){
-        if(auth()->user()->roleName() == 'client'){
+    public static function execute_saved_locations()
+    {
+        if (auth()->user()->roleName() == 'client') {
             $check_len = saved_locations::query()
-                ->where('user_id',auth()->id())
+                ->where('user_id', auth()->id())
                 ->count();
-            if($check_len >= 5){
+            if ($check_len >= 5) {
                 abort(Messages::error(__('errors.max_value')));
             }
         }
     }
 
-    public static function execute_saved_properties(){
-        if(auth()->user()->roleName() == 'client'){
+    public static function execute_saved_properties()
+    {
+        if (auth()->user()->roleName() == 'client') {
             $check_len = saved_properties_settings::query()
-                ->where('user_id',auth()->id())
+                ->where('user_id', auth()->id())
                 ->count();
-            if($check_len >= 5){
+            if ($check_len >= 2) {
                 abort(Messages::error(__('errors.max_value')));
             }
         }
