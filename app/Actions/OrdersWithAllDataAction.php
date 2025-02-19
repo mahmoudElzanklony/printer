e481487913ec507234ca43006e227005ebe7ea46
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-02-19 07:35:57 +0200

    edit order

--- app/Actions/OrdersWithAllDataAction.php
+++ app/Actions/OrdersWithAllDataAction.php
@@ -9,14 +9,16 @@ class OrdersWithAllDataAction
 {
     public static function get($is_order = true)
     {
-        return orders::query()
-            ->when($is_order == false, function ($query) {$query->where('status',OrderStatuesEnum::cart);})
-            ->with(['coupon_order.coupon','statues','items','rate'])
-            ->when($is_order == false, function ($query) {$query->with('payment');})
+        return orders::query()->with('payment')
+            ->when($is_order == false, function ($query) {
+                $query->where('status', OrderStatuesEnum::cart);
+            })
+            ->with(['coupon_order.coupon', 'statues', 'items', 'rate'])
+            //->when($is_order == false, function ($query) {$query->with('payment');})
             ->when(auth()->user()->roleName() != 'client',
-                fn($e)=> $e->with('user'))
+                fn ($e) => $e->with('user'))
             ->when(auth()->user()->roleName() == 'client',
-                fn($e)=> $e->where('user_id',auth()->id()))
-            ->orderBy('id','DESC');
+                fn ($e) => $e->where('user_id', auth()->id()))
+            ->orderBy('id', 'DESC');
     }
 }
