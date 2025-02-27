8db2bfcef9c57cc47de229d6f85bcff865cd0af2
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-02-27 02:44:14 +0200

    update payment observer and client note json

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -44,8 +44,6 @@ class OrdersController extends Controller
     {
         VerifyAccess::execute('pi pi-cart-plus|/orders|read');
         $data = OrdersWithAllDataAction::get();
-
-        return $data->get();
         $output = app(Pipeline::class)
             ->send($data)
             ->through([
