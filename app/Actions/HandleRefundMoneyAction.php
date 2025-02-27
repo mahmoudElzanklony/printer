332ab93aabff1f39f6a9c4dd30d6dc21e22d3613
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-02-27 02:39:56 +0200

    update payment observer and client note json

--- app/Actions/HandleRefundMoneyAction.php
+++ app/Actions/HandleRefundMoneyAction.php
@@ -9,17 +9,14 @@ class HandleRefundMoneyAction
         if ($order) {
             // Decode the existing note JSON
             $noteData = json_decode($order->note, true);
-            dd($order, $noteData);
+
             // Ensure the note is an array
-            if (! is_array($noteData)) {
-                $noteData = [];
-                // Update the system_refund key
-                $noteData['system_refund'] = (string) ($refund_money);
+            if (is_numeric($noteData['system_refund'])) {
+                $noteData['system_refund'] = $noteData['system_refund'] + ($refund_money);
             } else {
-                if (is_numeric($noteData['system_refund'])) {
-                    $noteData['system_refund'] = $noteData['system_refund'] + ($refund_money);
-                }
+                $noteData['system_refund'] = (string) ($refund_money);
             }
+            dd($order, $noteData);
 
             // Update the orders table
             $order->update(['note' => json_encode($noteData, JSON_UNESCAPED_UNICODE)]);
