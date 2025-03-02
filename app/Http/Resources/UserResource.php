e8d63d00b3074b0f98a053309f4a82a39388165b
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-03-02 16:41:14 +0200

    add image to user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Models\categories;
-use App\Models\saved_locations;
 use App\Models\saved_properties_settings;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -18,37 +16,38 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         $data = [
-            'id'=>$this->id,
-            'username'=>$this->username,
-            'email'=>$this->email,
-            'verified_at'=>$this->phone_verified_at,
-            'phone'=>$this->phone,
-            'otp'=>$this->otp_secret,
-            'has_password'=>strlen($this->password) > 0  ? true : false,
-            'wallet'=>$this->wallet,
-            'role'=>$this->roles->pluck('name')[0] ?? 'client',
-            'saved_properties_count'=>saved_properties_settings::query()->where('user_id',$this->id)->count(),
-            'created_at'=>$this->created_at->format('Y-m-d H:i:s')
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+            'verified_at' => $this->phone_verified_at,
+            'phone' => $this->phone,
+            'otp' => $this->otp_secret,
+            'has_password' => strlen($this->password) > 0 ? true : false,
+            'wallet' => $this->wallet,
+            'role' => $this->roles->pluck('name')[0] ?? 'client',
+            'saved_properties_count' => saved_properties_settings::query()->where('user_id', $this->id)->count(),
+            'image' => ImageResource::make($this->whenLoaded('image')),
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
         ];
-        if(isset($this->default_location)){
+        if (isset($this->default_location)) {
             $data['saved_location'] = SavedLocationResource::make($this->default_location);
         }
-        if(isset($this->default_category_id)){
+        if (isset($this->default_category_id)) {
             $data['default_category_id'] = $this->default_category_id;
         }
-        if(isset($this->default_service_id)){
+        if (isset($this->default_service_id)) {
             $data['default_service_id'] = $this->default_service_id;
         }
-        if(isset($this->default_country_id)){
+        if (isset($this->default_country_id)) {
             $data['default_country_id'] = $this->default_country_id;
         }
-        if(isset($this->token)){
+        if (isset($this->token)) {
             $data['token'] = $this->token;
         }
-        if(isset($this->token)){
+        if (isset($this->token)) {
             $data['token'] = $this->token;
         }
-        if(!($data['role'] == 'admin' || $data['role'] == 'client')){
+        if (! ($data['role'] == 'admin' || $data['role'] == 'client')) {
             $groupedPermissions = $this->getAllPermissions()->groupBy(function ($permission) {
                 // Extract the icon prefix (before "|/|")
                 return explode('|', $permission->name)[0];
@@ -56,12 +55,13 @@ class UserResource extends JsonResource
             $data['pages'] = $groupedPermissions->map(function ($permissions, $icon) {
                 return [
                     'icon' => $icon,
-                    'label'=>__('admin_nav.'.$icon)['label'],
-                    'parent'=>__('admin_nav.'.$icon)['parent'] ?? null,
+                    'label' => __('admin_nav.'.$icon)['label'],
+                    'parent' => __('admin_nav.'.$icon)['parent'] ?? null,
                     'permissions' => PermissionResource::collection($permissions),
                 ];
             })->values();
         }
+
         return $data;
     }
 }
