0128eea254f186deb885f217be4ef58f7c8c25f1
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-03-17 22:51:50 +0200

    add tax money

--- app/Actions/OrdersWithAllDataAction.php
+++ app/Actions/OrdersWithAllDataAction.php
@@ -13,7 +13,7 @@ class OrdersWithAllDataAction
             ->when($is_order == false, function ($query) {
                 $query->where('status', OrderStatuesEnum::cart);
             })
-            ->with(['coupon_order.coupon', 'statues', 'items.properties', 'rate'])
+            ->with(['coupon_order.coupon', 'statues', 'items.properties.property', 'rate'])
             //->when($is_order == false, function ($query) {$query->with('payment');})
             ->when(auth()->user()->roleName() != 'client',
                 fn ($e) => $e->with('user'))
