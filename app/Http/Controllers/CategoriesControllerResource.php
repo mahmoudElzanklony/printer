fd7eb8e3a310045282b47507f62c179dbf19da5c
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2025-09-18 10:23:48 +0300

    fix printer

--- app/Http/Controllers/CategoriesControllerResource.php
+++ app/Http/Controllers/CategoriesControllerResource.php
@@ -7,52 +7,52 @@ use App\Actions\VerifyAccess;
 use App\Http\Requests\categoriesFormRequest;
 use App\Http\Resources\CategoryResource;
 use App\Http\Resources\PropertyHeadingResource;
+use App\Http\Traits\upload_image;
 use App\Models\categories;
-use App\Models\categories_properties;
 use App\Models\properties;
 use App\Models\properties_heading;
 use App\Services\FormRequestHandleInputs;
 use App\Services\Messages;
-use Illuminate\Http\Request;
-use App\Http\Traits\upload_image;
 use Illuminate\Support\Facades\DB;
 
 class CategoriesControllerResource extends Controller
 {
     use upload_image;
+
     /**
      * Display a listing of the resource.
      */
     public function __construct()
     {
 
-        $this->middleware('auth:sanctum')->except('index','show');
-        $this->middleware('optional_auth')->only('index','show');
+        $this->middleware('auth:sanctum')->except('index', 'show');
+        $this->middleware('optional_auth')->only('index', 'show');
 
     }
+
     public function index()
     {
         VerifyAccess::execute('pi pi-sitemap|/categories|read');
-        $data = categories::query()->orderBy('id','DESC')->get();
+        $data = categories::query()->orderBy('id', 'DESC')->get();
         return CategoryResource::collection($data);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function save($data , $image)
+    public function save($data, $image)
     {
         DB::beginTransaction();
         // prepare data to be created or updated
         $data['user_id'] = auth()->id();
-        $data =  FormRequestHandleInputs::handle_inputs_langs($data,['name','info']);
+        $data = FormRequestHandleInputs::handle_inputs_langs($data, ['name', 'info']);
         // start save category data
         $category = categories::query()->updateOrCreate([
-            'id'=>$data['id'] ?? null
-        ],$data);
+            'id' => $data['id'] ?? null
+        ], $data);
         // check if there is any image related to this category and save it
-        if(!(array_key_exists('id',$data)) || (array_key_exists('id',$data) && $image != null)){
-            $this->check_upload_image($image,'categories',$category->id,'categories');
+        if (!(array_key_exists('id', $data)) || (array_key_exists('id', $data) && $image != null)) {
+            $this->check_upload_image($image, 'categories', $category->id, 'categories');
         }
         // save properties
         // Fetch the prices for the properties
@@ -67,13 +67,13 @@ class CategoriesControllerResource extends Controller
 
         DB::commit();
         // return response
-        return Messages::success(__('messages.saved_successfully'),CategoryResource::make($category));
+        return Messages::success(__('messages.saved_successfully'), CategoryResource::make($category));
     }
 
     public function store(categoriesFormRequest $request)
     {
         VerifyAccess::execute('pi pi-sitemap|/categories|create');
-        return $this->save($request->validated(),request()->file('image'));
+        return $this->save($request->validated(), request()->file('image'));
     }
 
     /**
@@ -82,35 +82,34 @@ class CategoriesControllerResource extends Controller
     public function show(string $id)
     {
         //
-        $cat  = categories::query()->where('id', $id)->FailIfNotFound(__('errors.not_found_data'));
+        $cat = categories::query()->where('id', $id)->FailIfNotFound(__('errors.not_found_data'));
 
 
         // i want to groupBy heading
         $data = categories::query()->with('properties')->find($id);
         ///////////////////////////////////////////// bad way
-        $headings_ids = $data->properties->map(function($e){
+        $headings_ids = $data->properties->map(function ($e) {
             return $e->property_id_heading;
         })->unique();
-        $properties_ids = $data->properties->map(function($e){
+        $properties_ids = $data->properties->map(function ($e) {
             return $e->id;
         })->unique();
-        $bad_way = properties_heading::query()->whereIn('id',$headings_ids)->with('properties',function($e) use ($properties_ids){
-            $e->whereIn('id',$properties_ids);
+        $bad_way = properties_heading::query()->whereIn('id', $headings_ids)->with('properties', function ($e) use ($properties_ids) {
+            $e->whereIn('id', $properties_ids);
         })->get();
-        return Messages::success('',['category-info'=>PropertyHeadingResource::collection($bad_way),'category'=>$cat]);
+        return Messages::success('', ['category-info' => PropertyHeadingResource::collection($bad_way), 'category' => $cat]);
         ///////////////////////////////////////////// bad way
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(categoriesFormRequest $request , $id)
+    public function update(categoriesFormRequest $request, $id)
     {
         VerifyAccess::execute('pi pi-sitemap|/categories|update');
-        return 123;
         $data = $request->validated();
         $data['id'] = $id;
-        return $this->save($data,request()->file('image'));
+        return $this->save($data, request()->file('image'));
     }
 
     /**
