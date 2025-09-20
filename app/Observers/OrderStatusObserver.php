442b9d0d79c4a64ba25a4a7aa3087c8e0cad09cb
Yosef Mekkawy <vosefmekkawy@gmail.com>
2025-09-20 23:03:50 +0300

    comment email in observers

--- app/Observers/OrderStatusObserver.php
+++ app/Observers/OrderStatusObserver.php
@@ -16,15 +16,16 @@ class OrderStatusObserver
      */
     public function created(orders_tracking $orders_tracking): void
     {
-        auth()->user()->notify(new OrderStatusNotification($orders_tracking));
+//        auth()->user()->notify(new OrderStatusNotification($orders_tracking));
 //         check if order tracking is cancelled
-        if($orders_tracking->status == OrderStatuesEnum::cancelled->value){
-            SendEmail::send('تم ارجاع المبلغ الخاص بالطلب الي محفظتك في '.env('APP_NAME'),
-                'تم ارجاع مبلغ قدرة '.$orders_tracking->order->payment->money.' الخاص بالطلب رقم '.$orders_tracking->order->id,
-                '','',$orders_tracking->order->user->email
-            );
-        }
+//        if($orders_tracking->status == OrderStatuesEnum::cancelled->value){
+//            SendEmail::send('تم ارجاع المبلغ الخاص بالطلب الي محفظتك في '.env('APP_NAME'),
+//                'تم ارجاع مبلغ قدرة '.$orders_tracking->order->payment->money.' الخاص بالطلب رقم '.$orders_tracking->order->id,
+//                '','',$orders_tracking->order->user->email
+//            );
+//        }
 
+//        dd('i am here in order tr observer');
         if ($orders_tracking->status->value == OrderStatuesEnum::completed->value) {
             CreateZohoInvoiceJob::dispatch($orders_tracking->order_id);
         }
