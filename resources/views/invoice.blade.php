9095989e861b48880346139ec89a75d68340c7c3
Yosef Mekkawy <vosefmekkawy@gmail.com>
2025-11-22 18:10:18 +0200

    updated invoice view

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -22,8 +22,7 @@
                 </div>
                 <div class="mt-3 mb-3">
                     <div class="p-2">
-                        <img class="d-block m-auto" style="width: 150px; "
-                             src="http://printer.first-meeting.net/images/logo.png">
+                        <img class="d-block m-auto" style="width: 150px;" src="{{ env('APP_LOGO_URL') }}">
                         <p class="mt-3 mb-2 font-weight-bold">
                             Bill Number{{--{{ __('keywords.bill_no') }}--}}
                             #{{ $order->id }}
